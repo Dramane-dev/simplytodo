a9d98c4adf291f86e777200a97afdd06d0f0bf83
Dramane <Dramane.dev@gmail.com>
2022-05-26 20:55:52 +0200

    config of angular app updated

--- simplytodo-front/karma.conf.js
+++ simplytodo-front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/todo-list-front'),
+      dir: require('path').join(__dirname, './coverage/simplytodo-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
